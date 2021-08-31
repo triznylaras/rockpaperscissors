a2b44dc981698f0dbf9a8f8b4a6ce02ce4a4042b
triznylaras <triznylaras@gmail.com>
2021-08-31 16:33:53 +0700

    Fix indentation to 2 spaces

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,26 +1,26 @@
 const rpsArray = ["Rock", "Paper", "Scissors"];
 
-function computerPlay () {
-    return rpsArray[~~(Math.random() * rpsArray.length)];
+function computerPlay() {
+  return rpsArray[~~(Math.random() * rpsArray.length)];
 }
 
-function playRound (playerSelection, computerSelection) {
-    if ((playerSelection == "rock" && computerSelection == "paper") || 
+function playRound(playerSelection, computerSelection) {
+  if ((playerSelection == "rock" && computerSelection == "paper") ||
     (playerSelection == "scissors" && computerSelection == "rock") ||
     (playerSelection == "paper" && computerSelection == "scissors")) {
-        return("You Lose! " + computerSelection + " beat " + playerSelection);
-    } else if ((playerSelection == "paper" && computerSelection == "rock") || 
+    return ("You Lose! " + computerSelection + " beat " + playerSelection);
+  } else if ((playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "paper")){
-        return("You WIN!");
-    } else if (playerSelection == computerSelection) {
-        return("Tie Game!");
-    } else {
-        return("Please try again");
-    }
+    (playerSelection == "scissors" && computerSelection == "paper")) {
+    return ("You WIN!");
+  } else if (playerSelection == computerSelection) {
+    return ("Tie Game!");
+  } else {
+    return ("Please try again");
+  }
 }
 
-let user = window.prompt ("Enter your RPS: ");
+let user = window.prompt("Enter your RPS: ");
 let userInput = user.toLowerCase();
 let computerInput = computerPlay().toLowerCase();
 let output = playRound(userInput, computerInput);
